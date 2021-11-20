b148f403c423e73e4be830674423d1da04f7e6aa
Angelina Fu <fuhanya.cn@gmail.com>
2021-11-20 22:21:44 +0800

    Move games example to different folder

--- PythonLab/examples/games/hello.py
+++ PythonLab/examples/games/hello.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World!')
-while True: # main game loop
+while True:  # main game loop
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
